27367392f157654ab4725e9e1041ec290915119c
Marina Fontenele <marinagomesfontenele2007@gmail.com>
2026-08-27 08:51:30 -0300

    Add files via upload

---
+++ FirstTable/FirstTable/FirstTableApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstTableApp.swift
+//  FirstTable
+//
+//  Created by Marina Gomes Fontenele on 27/08/26.
+//
+
+import SwiftUI
+
+@main
+struct FirstTableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
