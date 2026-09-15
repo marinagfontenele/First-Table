1f83ac88e4d47b6589291e830ec74cd755743de6
Marina Gomes Fontenele <marinagomesfontenele2007@gmail.com>
2026-09-15 10:51:55 -0300

    feat: OnboardingView VoiceOver

--- FirstTable/FirstTable/View/OnboardingView.swift
+++ FirstTable/FirstTable/View/OnboardingView.swift
@@ -24,6 +24,7 @@ struct OnboardingView: View {
                             ViewThatFits(in: .vertical) {
                                 VStack {
                                     Image(option.imageName)
+                                        .accessibilityHidden(true)
                                     
                                     Spacer()
                                     
@@ -31,15 +32,20 @@ struct OnboardingView: View {
                                         .font(Font.custom("Poppins-SemiBold", size: 32))
                                         .padding(.bottom, 8)
                                         .multilineTextAlignment(.center)
+                                        .accessibilitySortPriority(3)
+
                                     
                                     Text(option.subtitle)
                                         .font(Font.custom("Poppins-Regular", size: 20))
                                         .padding(.bottom, 50)
                                         .multilineTextAlignment(.center)
+                                        .accessibilitySortPriority(2)
+
                                 }
                                 
                                 ScrollView (showsIndicators: false){
                                     Image(option.imageName)
+                                        .accessibilityHidden(true)
                                     
                                     Spacer()
                                     
@@ -47,11 +53,15 @@ struct OnboardingView: View {
                                         .font(Font.custom("Poppins-SemiBold", size: 32))
                                         .padding(.bottom, 8)
                                         .multilineTextAlignment(.center)
+                                        .accessibilitySortPriority(1)
+
                                     
                                     Text(option.subtitle)
                                         .font(Font.custom("Poppins-Regular", size: 20))
                                         .padding(.bottom, 50)
                                         .multilineTextAlignment(.center)
+                                        .accessibilitySortPriority(2)
+
                                 }
                             }
 
@@ -95,6 +105,8 @@ struct OnboardingView: View {
                 }
                 .padding(.horizontal, 40)
                 .padding(.vertical, 8)
+                .accessibilitySortPriority(3)
+
                 
                 Spacer()
             } else {
@@ -106,6 +118,8 @@ struct OnboardingView: View {
                 }
                 .padding(.horizontal, 40)
                 .padding(.vertical, 8)
+                .accessibilitySortPriority(1)
+
                 
                 Spacer()
             }
