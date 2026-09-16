e91b0a54f5aec45189e375831cfe231e14edf392
Marina Gomes Fontenele <marinagomesfontenele2007@gmail.com>
2026-09-16 11:51:37 -0300

    feat: FoundationUnknowView

---
+++ FirstTable/FirstTable/View/FoundationUnknownView.swift
@@ -0,0 +1,74 @@
+//
+//  FoundationUnknowView.swift
+//  FirstTable
+//
+//  Created by Marina Gomes Fontenele on 16/09/26.
+//
+
+import SwiftUI
+
+struct FoundationUnknowView: View {
+    var body: some View {
+        ViewThatFits (in: .vertical) {
+            VStack {
+                Spacer()
+                
+                Image("errorImage")
+                    .scaledToFit()
+                
+                Text("Ocorreu um erro inesperado com a Apple Intelligence")
+                    .font(.custom("Poppins-Bold", size: 24))
+                    .padding()
+                
+                Text("Aguarde alguns instantes e tente novamente depois.")
+                    .font(.custom("Poppins-Medium", size: 16))
+                    .padding(.bottom)
+                    .padding(.horizontal)
+                
+                Spacer()
+                
+                Image("cryingOllie")
+                    .padding(.bottom, -270)
+                    .scaledToFit()
+            }
+            .padding(.horizontal, 27)
+            .background(Color.bgBlack.ignoresSafeArea(edges: .all))
+            .multilineTextAlignment(.center)
+            
+            ScrollView (showsIndicators: false){
+                VStack {
+                    Spacer(minLength: 30)
+                    
+                    Image("errorImage")
+                        .scaledToFit()
+                    
+                    Text("Ocorreu um erro inesperado com a Apple Intelligence")
+                        .font(.custom("Poppins-Bold", size: 24))
+                        .padding()
+                    
+                    Text("Aguarde alguns instantes e tente novamente depois.")
+                        .font(.custom("Poppins-Medium", size: 16))
+                        .padding(.bottom)
+                        .padding(.horizontal)
+                    
+                    Spacer(minLength: 30)
+                    
+                    Image("cryingOllie")
+                        .padding(.bottom, -270)
+                        .scaledToFit()
+                }
+                .frame(minWidth: 400, maxWidth: 700)
+                .multilineTextAlignment(.center)
+                .padding(.horizontal, 20)
+                .padding(.horizontal, 30)
+            }
+            .padding(.horizontal)
+        }
+        .background(Color.bgBlack.ignoresSafeArea(edges: .all))
+    }
+}
+
+#Preview {
+    FoundationUnknowView()
+}
+
